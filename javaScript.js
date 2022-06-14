abd4307b93413eefd30617d09629282280e08f09
Brady Barker <brady.d.barker@gmail.com>
2022-06-14 15:06:43 -0400

    Complete computerPlay() function Returns rock paper or scissors based on random number (1-3) generated

--- javaScript.js
+++ javaScript.js
@@ -1,5 +1,29 @@
 /*
 #! JavaScript
 Rock Paper Scissors game played against computer
-
 */
+
+/*
+Generate a random number from: 1,2,3
+1 = rock, 2 = paper, 3 = scissors
+return rock, paper, or scissors
+*/
+function computerPlay() {
+    randomNum = Math.floor(Math.random()*3)+1
+    if (randomNum == 1) {
+        return 'rock'
+    }else if (randomNum == 2) {
+        return 'paper'
+    }else {
+        return 'scissors'
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+    return null
+}
+
+const playerSelection = 'rock';
+const computerSelection = computerPlay();
+
+console.log(computerSelection)

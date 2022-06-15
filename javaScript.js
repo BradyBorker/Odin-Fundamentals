1c37d3b472363f6615462ce51d83552af5b2b783
Brady Barker <brady.d.barker@gmail.com>
2022-06-15 10:53:09 -0400

    getUserInput function created Takes a user input and validates it to ensure that it is: rock, paper, or scissors. Can take any formatting of those values and still function. Asks the question multiple times until correct value(s) given.

--- javaScript.js
+++ javaScript.js
@@ -40,20 +40,42 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
+function getUserInput() {
+    while (true) {
+        input = prompt('Rock, Paper, or Scissors?')
+        if (input.toLowerCase() == 'rock' || input.toLowerCase() == 'paper'
+            || input.toLowerCase() == 'scissors') {
+                break
+        }else {
+            continue
+        }
+    }
+    return input.toLowerCase()
+}
+
 function game() {    
-    let win, lose, tie = 0;
+    let win = lose = tie = 0;
     for (let i=0; i<5; i++) {
-        const playerSelection = 'rock';
+        const playerSelection = getUserInput();
         const computerSelection = computerPlay();
-        //console.log(`Round ${i+1} results: ` + playRound(playerSelection, computerSelection))
         results = playRound(playerSelection, computerSelection)
-        if (results) {
+        if (results == true) {
             console.log(`Round ${i+1} results: ` + `You Win! ${playerSelection} beats ${computerSelection}`)
+            win += 1;
         }else if (results == "tie") {
             console.log(`Round ${i+1} results: ` + 'It is a tie!')
+            tie += 1;
         }else {
             console.log(`Round ${i+1} results: ` + `You Lose! ${computerSelection} beats ${playerSelection}`)
+            lose += 1;
         }
+    }
+    console.log(`win: ${win} | lose: ${lose} | tie: ${tie}`)
+    if (win > lose) {
+        console.log('YOU WIN!')
+    }
+    else {
+        console.log('YOU LOSE!')
     }
     
 }

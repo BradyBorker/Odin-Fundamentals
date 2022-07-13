c7f52b447cd8333820b1d9f5a26cf105624ba056
Brady Barker <brady.d.barker@gmail.com>
2022-07-13 17:59:54 -0400

    Listens for mouse click, uses previous logic to determine if win or loss. If win increase win textContent, if lose increase lose textContent, if tie increase tie textContent

--- jsRPS.js
+++ jsRPS.js
@@ -8,7 +8,7 @@ Generate a random number from: 1,2,3
 1 = rock, 2 = paper, 3 = scissors
 return rock, paper, or scissors
 */
-function computerPlay() {
+function computerChoice() {
     randomNum = Math.floor(Math.random()*3)+1
     if (randomNum == 1) {
         return 'rock'
@@ -36,44 +36,34 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-function getUserInput() {
-    while (true) {
-        input = prompt('Rock, Paper, or Scissors?')
-        if (input.toLowerCase() == 'rock' || input.toLowerCase() == 'paper'
-            || input.toLowerCase() == 'scissors') {
-                break
-        }else {
-            continue
-        }
-    }
-    return input.toLowerCase()
-}
+function game(playerSelection) {    
+    
+    const computerSelection = computerChoice();
+    results = playRound(playerSelection, computerSelection)
 
-function game() {    
-    let win = lose = tie = 0;
-    for (let i=0; i<5; i++) {
-        const playerSelection = getUserInput();
-        const computerSelection = computerPlay();
-        results = playRound(playerSelection, computerSelection)
-        if (results == true) {
-            console.log(`Round ${i+1} results: ` + `You Win! ${playerSelection} beats ${computerSelection}`)
-            win += 1;
-        }else if (results == "tie") {
-            console.log(`Round ${i+1} results: ` + 'It is a tie!')
-            tie += 1;
-        }else {
-            console.log(`Round ${i+1} results: ` + `You Lose! ${computerSelection} beats ${playerSelection}`)
-            lose += 1;
-        }
-    }
-    console.log(`win: ${win} | lose: ${lose} | tie: ${tie}`)
-    if (win > lose) {
-        console.log('YOU WIN!')
-    }
-    else {
-        console.log('YOU LOSE!')
+    const win = document.querySelector('.win');
+    const lose = document.querySelector('.lose');
+    const tie = document.querySelector('.tie');
+    
+    if (results == true) {
+        win.textContent = (Number(win.textContent)+1).toString();
+    }else if (results == "tie") {
+        tie.textContent = (Number(tie.textContent)+1).toString();
+    }else {
+        lose.textContent = (Number(lose.textContent)+1).toString();
     }
     
 }
 
-game()
+// Wait for user click to select rock, paper, or scissor
+// Use input to play game
+
+const userInput = document.querySelectorAll(".user-cards div")
+userInput.forEach((card) => {
+    
+    card.addEventListener('click', (e) => {
+        playerSelection = e.target.className
+        game(playerSelection)
+    })
+
+})

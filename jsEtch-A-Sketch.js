fdee7d69692f7919cd9193c1643edc9c5ea7c2d4
Brady Barker <brady.d.barker@gmail.com>
2022-07-16 21:16:35 -0400

    Click and drag works on grid. Added color selection div Added grid size slider to color selection div Grid slider working

--- jsEtch-A-Sketch.js
+++ jsEtch-A-Sketch.js
@@ -1,7 +1,7 @@
 // Generate DIVs for grid
 // Make "value x value" amount of div
 function divGen(value) {
-    const container = document.querySelector('.container');
+    const container = document.querySelector('.grid-container');
     for (let i=0; i<value; i++) {
         const row = document.createElement('div');
         row.classList.add("row");
@@ -14,4 +14,24 @@ function divGen(value) {
         container.appendChild(row);
     }
 }
-divGen(28);
+// TODO
+// Slider/ User Input for number of divs
+divGen(20);
+
+// Function that will give divs clicked and dragged color.
+const cell = document.querySelectorAll('.gridsquare');
+cell.forEach((cell) => {
+    cell.addEventListener('mousemove', (e) => {
+        if (e.buttons == 1) { // If the mouse is down   
+        // TODO:
+        // Grab color from user
+            e.target.style.background = 'red';
+        }
+    })
+})
+
+/* slider */
+const slider = document.querySelector('#myRange');
+slider.addEventListener('input', () => {
+    console.log(slider.value)
+})
